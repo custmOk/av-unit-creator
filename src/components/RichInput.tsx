535eefb968876ecd2c9b727cb0c3baafd2e5075b
Ryan Powell <ryanpowell0913@gmail.com>
2026-01-27 18:41:44 -0500

    added another coloring option

--- src/components/RichInput.tsx
+++ src/components/RichInput.tsx
@@ -34,7 +34,8 @@ export const RichInput: React.FC<Props> = ({
                 { label: "Conditional", color: '#6BF5FA' },
                 { label: "When 'x' Happens", color: '#FF7B1C' },
                 { label: 'PvP Only', color: '#30C23D' },
-                { label: 'References', color: '#0BADFF' }
+                { label: 'References', color: '#0BADFF' },
+                { label: 'Max Values', color: '#909090' }
             ],
         },
         {
