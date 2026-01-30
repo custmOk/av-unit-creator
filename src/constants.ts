83d8433f42bcd63b791561eba5e29e19abc62f72
Ryan Powell <ryanpowell0913@gmail.com>
2026-01-30 01:46:14 -0500

    another fix

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-const adminEmails = import.meta.env.VITE_ADMIN_EMAILS;
+const adminEmails: string[] = import.meta.env.VITE_ADMIN_EMAILS?.split(',') || [];
 
 export const isAdmin = (session: any) => {
     return adminEmails.includes(session?.user?.email);
